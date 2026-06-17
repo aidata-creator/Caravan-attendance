9f9b01a3959c2929a66d6cd01228fbe9dfd02d34
aidata-creator <aidata@hapinoy.com>
2026-06-17 15:19:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ if uploaded_file is not None:
             for attempt in range(max_retries):
                 try:
                     response = client.models.generate_content(
-                        model='gemini-2.5-flash',
+                        model='gemini-3-flash-preview',
                         contents=[optimized_image, prompt],
                         config=types.GenerateContentConfig(
                             response_mime_type="application/json",
